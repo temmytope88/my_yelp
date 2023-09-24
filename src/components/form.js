74ad7abf131ffd6ff39d8c2f8cf97894cfb05459
root <root@DESKTOP-NFFQUMS>
2023-09-24 05:40:48 +0100

    initial commit

---
+++ src/components/form.js
@@ -0,0 +1,22 @@
+
+const Form = () =>{
+    return(
+        <form>
+            <p>
+                <label>Restaurant:</label><br/>
+                <input></input>
+            </p>
+            <p>
+                <label>Description:</label><br/>
+                <input></input>
+            </p>
+            <p>
+                <label>Location:</label><br/>
+                <input></input>
+            </p>
+            <button>Submit</button>        
+        </form> 
+    )
+}
+
+export default Form
---
+++ src/components/table.js
@@ -0,0 +1,27 @@
+const Table = () =>{
+    return(
+        <table>
+            <thead>
+                <tr>
+                    <th>Restaurant</th>
+                    <th>Description</th>
+                    <th>Location</th>
+                </tr>
+            </thead>  
+            <tbody>
+                <tr>
+                    <td>Spices</td>
+                    <td>Fast food</td>
+                    <td>Lagos</td>
+                </tr>
+                <tr>
+                    <td>Spices</td>
+                    <td>Fast food</td>
+                    <td>Lagos</td>
+                </tr>
+            </tbody>      
+        </table>
+    )
+}
+
+export default Table
